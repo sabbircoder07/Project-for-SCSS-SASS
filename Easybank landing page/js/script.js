bc557e7b58b3af8b1838908756db224cfe1982eb
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2023-05-02 08:47:23 +0600

    Create script.js

---
+++ Easybank landing page/js/script.js
@@ -0,0 +1,15 @@
+const iconHamburger = document.querySelector(".icon-hamburger");
+const navBarMobile = document.querySelector(".nav-bar-mobile");
+const iconClose = document.querySelector(".icon-close");
+const mobleNavPageLink = document.querySelectorAll(".moble-nav__link");
+
+iconHamburger.addEventListener("click", function () {
+  navBarMobile.classList.toggle("nav-mobile-show");
+});
+
+/*
+iconClose.addEventListener("click", function () {
+  navBarMobile.classList.remove("nav-mobile-show");
+  navBarMobile.classList.add("nav-mobile-hide");
+});
+*/
